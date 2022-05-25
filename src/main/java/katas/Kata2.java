b2c642a63866ff00df033128097e572c065a6f8f
Aura Russill <auracris1996@hotmail.com>
2022-05-25 11:28:02 -0500

    Kata 2
    Modificacion de la Kata 21, con el test pasado

--- src/main/java/katas/Kata2.java
+++ src/main/java/katas/Kata2.java
@@ -1,12 +1,11 @@
 package katas;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import model.Movie;
 import util.DataUtil;
 
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Chain filter() and map() to collect the ids of videos that have a rating of 5.0
@@ -16,7 +15,11 @@ import java.util.Map;
 public class Kata2 {
     public static List<Integer> execute() {
         List<Movie> movies = DataUtil.getMovies();
+        List<Integer> peliculas = movies.stream().map(
+                pelis -> pelis.getRating())
+        .filter(raiting -> raiting == 5.0).map(valor -> valor.intValue()).collect(Collectors.toList());
 
-        return ImmutableList.of(1, 2, 3);
+//        ImmutableList.of(1, 2, 3)
+        return peliculas;
     }
 }
--- src/test/java/katas/Kata1Test.java
+++ src/test/java/katas/Kata1Test.java
@@ -1,5 +1,6 @@
 package katas;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import static org.hamcrest.CoreMatchers.*;
 
@@ -7,12 +8,15 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.Map;
 
 
 public class Kata1Test {
 
     @Test
     public void testExecute() {
+//        valorEsperado = ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys");
+//        valorActual = Kata1.execute().forEach((elemento -> elemento.get(Map.of())));
         Assert.assertThat(Kata1.execute().size(), equalTo(4));
     }
 }
